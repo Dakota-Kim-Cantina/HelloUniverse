c4ec25ea23a7e1dc266789eddd1489d944b67ed4
Dakota Kim <dakota@cantina.co>
2019-08-16 13:42:43 -0400

    set onclicklistener

--- app/src/main/java/com/dakotakim/hellouniverse/MainActivity.kt
+++ app/src/main/java/com/dakotakim/hellouniverse/MainActivity.kt
@@ -2,11 +2,22 @@ package com.dakotakim.hellouniverse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val greetingText = findViewById<TextView>(R.id.greeting_text)
+        val personButton = findViewById<EditText>(R.id.person_button)
+        val actionGreet = findViewById<Button>(R.id.action_greet)
+
+        // Lambda syntax
+        actionGreet.setOnClickListener { btn ->
+            greetingText.text = "Hello, ${personButton.text}"
+        }
     }
 }
